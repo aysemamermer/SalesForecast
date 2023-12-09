9d60efe78afd1e0bacd737016041d412bf3240a8
aysema mermer <aysemamermer99@gmail.com>
2023-12-09 09:05:31 +0300

    feat: selim.py batu.py added

--- batu.py
+++ batu.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """
-assad
+1- Prophet
+2- stats model
 """
+import pandas as pd
+pd.set_option("display.max_columns", None)
+df = pd.read_excel('new_filtered.xlsx')
+
+print(df.head())
--- selim.py
+++ selim.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+1- Tensorflow
+"""
 
-"""
-assad
-"""
+import pandas as pd
+pd.set_option("display.max_columns", None)
+df = pd.read_excel('new_filtered.xlsx')
+
+print(df.head())
